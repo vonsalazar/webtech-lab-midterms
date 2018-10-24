32a366a4bd622e787c57e4452bac6688f66c332a
JC_SLU Github <35597610+JCSLUGithub@users.noreply.github.com>
2018-10-24 22:47:31 +0800

    Added the web app manifest and sample content
    Added the web app manifest which describes the web application.
    Sample content which describes what the web application will do (application features) are added as well as sample images.

--- Progressive Web Application/serviceworker.js
+++ Progressive Web Application/serviceworker.js
@@ -2,27 +2,41 @@ var cacheName = 'pwa';
 var cacheTargets = [
     '/',
     '/index.html',
-    '/images/sample.jpg',
+    '/images/homeicon.png',
+    '/images/homeicon128.png',
+    '/images/homeicon64.png',
+    '/images/homeicon32.png',
+    '/images/sample1.png',
+    '/images/sample2.png',
+    '/images/sample3.png',
     '/scripts/app.js',
     '/styles/main.css'
 ];
 
 self.addEventListener('install', function(e) {
+
     console.log('[Service Worker] Installing...');
+
     e.waitUntil(
+
         caches.open(cacheName).then(function(cache) {
+
             console.log('[Service Worker] Caching...');
             return cache.addAll(cacheTargets);
         })
     );
-    
 });
 
 self.addEventListener('activate', function(e) {
+
     console.log('[Service Worker] Activated');
+
     e.waitUntil(
+
         caches.keys().then(function(keyList) {
+
             return Promise.all(keyList.map(function(key) {
+
                 if (key !== cacheName) {
                     console.log('[Service Worker] Removing old cache', key);
                     return caches.delete(key);
@@ -34,10 +48,18 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
+
     console.log('[Service Worker] Fetching from cache...', e.request.url);
+
     e.respondWith(
+
         caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
+
+            if (response) {
+                console.log("[Service Worker] Exists in cache")
+                return response;
+            }
+            return fetch(e.request);
         })
     )
 })
