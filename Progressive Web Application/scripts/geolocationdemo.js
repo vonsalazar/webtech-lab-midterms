a9cf80b1c1237c393ba2d91bd927286d4862e3a4
JC_SLU Github <35597610+JCSLUGithub@users.noreply.github.com>
2018-11-04 23:24:21 +0800

    Updates to the location and geolocation functionalities.
    Minor updates on content formatting and styling.
    Completed the location functionality for the public safety services page.
    Added the geolocationdemo.js for testing of geolocation functionality.

---
+++ Progressive Web Application/scripts/geolocationdemo.js
@@ -0,0 +1,101 @@
+var response = document.getElementById('geolocationModule');
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(userLocation);
+    } else {
+        response.innerHTML = 'There was an error processing the request.';
+    };
+};
+
+function userLocation(position) {
+    nearestLocation(position.coords.latitude, position.coords.longitude);
+    //response.innerHTML = 'Latitude: ' + position.coords.latitude + '<br>Longitude: ' + position.coords.longitude;
+};
+
+function DegToRad(deg) {
+    return deg * Math.PI / 180;
+};
+
+function pythagorasEquirectangular(lat1, lon1, lat2, lon2) {
+    lat1 = DegToRad(lat1);
+    lat2 = DegToRad(lat2);
+    lon1 = DegToRad(lon1);
+    lon2 = DegToRad(lon2);
+    var R = 6371;
+    var x = (lon2 - lon1) * Math.cos((lat1 + lat2) / 2);
+    var y = (lat2 - lat1);
+    var d = Math.sqrt(x * x + y * y) * R;
+    return d;
+};
+
+function nearestLocation(latitude, longitude) {
+    var mindif = 99999;
+    var closestIndex;
+    var closestLocation;
+    //var closestName, closestAddress, closestContact;
+
+    for (index = 0; index < locations.length; index++) {
+        var dif = pythagorasEquirectangular(latitude, longitude, locations[index][4], locations[index][5]);
+        if (dif < mindif) {
+            //closestName = index[0];
+            //closestAddress = index[1];
+            //closestContact = index[2];
+            closestIndex = index;
+            closestLocation = locations[closestIndex];
+            mindif = dif;
+        };
+    };
+    //contactInfo = document.createElement('p');
+
+    //nameText = document.createTextNode(cursor.value.name);
+    //contactInfo.appendChild(contactInfoText);
+
+    //locationListItem.appendChild(contactInfo);
+
+    //document.getElementById('baguioLocationList').appendChild(locationListItem);
+
+    console.log(closestLocation[1]);
+
+    var locName, locAddressHeader, locAddress, locContactHeader, locContact, locNameText, locAddressHeaderText, locAddressText, locContactHeaderText, locContactText;
+
+    locName = document.createElement('h3');
+    locAddressHeader = document.createElement('h4');
+    locAddress = document.createElement('p');
+    locContactHeader = document.createElement('h4');
+    locContact = document.createElement('p');
+
+    locNameText = document.createTextNode(closestLocation[0]);
+    locAddressHeaderText = document.createTextNode('Address:');
+    locAddressText = document.createTextNode(closestLocation[1]);
+    locContactHeaderText = document.createTextNode('Contact Number:');
+    locContactText = document.createTextNode(closestLocation[2]);
+
+    locName.appendChild(locNameText);
+    locAddressHeader.appendChild(locAddressHeaderText);
+    locAddress.appendChild(locAddressText);
+    locContactHeader.appendChild(locContactHeaderText);
+    locContact.appendChild(locContactText);
+
+    document.getElementById('geolocationModule').appendChild(locName);
+    document.getElementById('geolocationModule').appendChild(locAddressHeader);
+    document.getElementById('geolocationModule').appendChild(locAddress);
+    document.getElementById('geolocationModule').appendChild(locContactHeader);
+    document.getElementById('geolocationModule').appendChild(locContact);
+
+    alert(
+        'Nearest emergency center: ' +
+        '\nLocation Name: ' + closestLocation[0] +
+        '\nAddress: ' + closestLocation[1] +
+        '\nContact Number: ' + closestLocation[2]
+        );
+
+};
+
+var locations = [
+    [ 'Hospital of the Sacred Heart', 'Assumption Road', '(074) 442-5701 to 02', 'images/locations/hospitalsample.jpg', 16.417309410344853, 120.59795916080475 ],
+    [ 'Baguio City Police Station 1', 'Naguilian Road', '424-2697', 'images/locations/policestationsample.jpg', 16.412449, 120.579358 ],
+    [ 'Baguio General Hospital', 'Baguio General Hospital Driveway', '(074) 661-7910', 'images/locations/baguiogeneralhospital.jpg', 16.40107403869452, 120.59601187705995 ],
+    [ 'Benguet General Hospital', 'Halsema Highway, La Trinidad', '(074) 442-3165', 'images/locations/benguetgeneralhospital.jpg', 16.450765, 120.589133 ],
+    [ 'La Trinidad Fire Station', 'Cabanao Road, La Trinidad', '(074) 422-4700', 'images/locations/firestationsample.jpg', 16.460805, 120.588766 ]
+];
--- Progressive Web Application/scripts/location.js
+++ Progressive Web Application/scripts/location.js
@@ -10,43 +10,36 @@ var database;
 var transaction;
 var DBRequest = self.IndexedDB.open('LocationDB', 1);
 
-/**
-var name, address, area, contact_main, contact_alt, image, x_coord, y_coord;
-var location = [
-    { locationID: 0, name: '', address: '', area: '', contact_main: '', contact_alt: '', image: '', x_coord: '', y_coord: ''}
-];
-*/
-
 var locationsBaguio = [
-    { locationID: 1, name: 'Baguio General Hospital', address: 'Baguio General Hospital Driveway', contact: '(074) 661-7910', image: '', x_coord: '16.40107403869452', y_coord: '120.59601187705995'},
-    { locationID: 2, name: 'Baguio Medical Center', address: '9 Military Cutoff Road', contact: '(074) 304-3873', image: '', x_coord: '16.4019900481756', y_coord: '120.59747099876405'},
-    { locationID: 3, name: 'Hospital of the Sacred Heart', address: 'Assumption Road', contact: '(074) 442-5701 to 02', image: '', x_coord: '16.417309410344853', y_coord: '120.59795916080475'},
-    { locationID: 4, name: 'Notre Dame de Chartres', address: '25 Main Building, General Luna Road', contact: '(074) 619-8530', image: '', x_coord: '16.41535917898586', y_coord: '120.59850096702577'},
-    { locationID: 5, name: 'Assumption Medical Diagnostic Center, Inc.', address: 'VFM Building, 10 Assumption Road', contact: '(074) 443-9580', image: '', x_coord: '16.41360875', y_coord: '120.59738539604751'},
-    { locationID: 6, name: 'Pines City Doctors Hospital', address: 'Magsaysay Avenue', contact: '(074) 445-3020', image: '', x_coord: '16.4269155', y_coord: '120.59469167932693'},
-    { locationID: 7, name: 'Philippine Red Cross', address: '39 Harrison Road', contact: '(074) 424-7953', image: '', x_coord: '16.4122177', y_coord: '120.5963337'},
-    { locationID: 8, name: 'Baguio City Police Station 1', address: 'Naguilian Road', contact: '424-2697', image: '', x_coord: '16.412449', y_coord: '120.579358' },
-    { locationID: 9, name: 'Baguio City Police Station 2', address: 'Camdas', contact: '661-1255', image: '', x_coord: '16.425757', y_coord: '120.593742' },
-    { locationID: 10, name: 'Baguio City Police Station 3', address: 'Pacdal Circle', contact: '300-9113 / 424-0670', image: '', x_coord: '16.416707', y_coord: '120.615552' },
-    { locationID: 11, name: 'Baguio City Police Station 4', address: 'PEZA, Loakan', contact: '305-9114 / 424-0992', image: '', x_coord: '16.379593', y_coord: '120.619338' },
-    { locationID: 12, name: 'Baguio City Police Station 5', address: 'Legarda, Marcos Highway', contact: '', image: '', x_coord: '16.401514', y_coord: '120.593555' },
-    { locationID: 13, name: 'Baguio City Police Station 6', address: 'Aurora Hill', contact: '300-9116 / 424-2174', image: '', x_coord: '16.426235', y_coord: '120.606191' },
-    { locationID: 14, name: 'Baguio City Police Station 7', address: '', contact: 'Central Business District', image: '', x_coord: '16.414404', y_coord: '120.592207' },
-    { locationID: 15, name: 'Baguio City Police Station 8', address: 'Camp 7, Kennon Road', contact: '424-2681', image: '', x_coord: '16.391862', y_coord: '120.599899' },
-    { locationID: 16, name: 'Baguio City Police Station 9', address: 'Irisan', contact: '424-8834', image: '', x_coord: '16.430057', y_coord: '120.548613' },
-    { locationID: 17, name: 'Baguio City Police Station 10', address: 'Green Valley, Marcos Highway', contact: '422-2662', image: '', x_coord: '16.388915', y_coord: '120.575467' },
-    { locationID: 18, name: 'Baguio City Central Fire Station', address: 'No. 1, Corner Abanao and Kayang Streets', contact: '160 / (074) 442-2222 / (074) 443-7089', image: '', x_coord: '16.414757', y_coord: '120.591727' }
+    { locationID: 1, name: 'Baguio General Hospital', address: 'Baguio General Hospital Driveway', contact: '(074) 661-7910', image: 'images/locations/baguiogeneralhospital.jpg', x_coord: '16.40107403869452', y_coord: '120.59601187705995'},
+    { locationID: 2, name: 'Baguio Medical Center', address: '9 Military Cutoff Road', contact: '(074) 304-3873', image: 'images/locations/baguiomedcenter.jpg', x_coord: '16.4019900481756', y_coord: '120.59747099876405'},
+    { locationID: 3, name: 'Hospital of the Sacred Heart', address: 'Assumption Road', contact: '(074) 442-5701 to 02', image: 'images/locations/hospitalsample.jpg', x_coord: '16.417309410344853', y_coord: '120.59795916080475'},
+    { locationID: 4, name: 'Notre Dame de Chartres', address: '25 Main Building, General Luna Road', contact: '(074) 619-8530', image: 'images/locations/hospitalsample.jpg', x_coord: '16.41535917898586', y_coord: '120.59850096702577'},
+    { locationID: 5, name: 'Assumption Medical Diagnostic Center, Inc.', address: 'VFM Building, 10 Assumption Road', contact: '(074) 443-9580', image: 'images/locations/hospitalsample.jpg', x_coord: '16.41360875', y_coord: '120.59738539604751'},
+    { locationID: 6, name: 'Pines City Doctors Hospital', address: 'Magsaysay Avenue', contact: '(074) 445-3020', image: 'images/locations/hospitalsample.jpg', x_coord: '16.4269155', y_coord: '120.59469167932693'},
+    { locationID: 7, name: 'Philippine Red Cross', address: '39 Harrison Road', contact: '(074) 424-7953', image: 'images/locations/hospitalsample.jpg', x_coord: '16.4122177', y_coord: '120.5963337'},
+    { locationID: 8, name: 'Baguio City Police Station 1', address: 'Naguilian Road', contact: '424-2697', image: 'images/locations/policestationsample.jpg', x_coord: '16.412449', y_coord: '120.579358' },
+    { locationID: 9, name: 'Baguio City Police Station 2', address: 'Camdas', contact: '661-1255', image: 'images/locations/policestationsample.jpg', x_coord: '16.425757', y_coord: '120.593742' },
+    { locationID: 10, name: 'Baguio City Police Station 3', address: 'Pacdal Circle', contact: '300-9113 / 424-0670', image: 'images/locations/policestationsample.jpg', x_coord: '16.416707', y_coord: '120.615552' },
+    { locationID: 11, name: 'Baguio City Police Station 4', address: 'PEZA, Loakan', contact: '305-9114 / 424-0992', image: 'images/locations/policestationsample.jpg', x_coord: '16.379593', y_coord: '120.619338' },
+    { locationID: 12, name: 'Baguio City Police Station 5', address: 'Legarda, Marcos Highway', contact: '442-0629 / 300-9115', image: 'images/locations/policestationsample.jpg', x_coord: '16.401514', y_coord: '120.593555' },
+    { locationID: 13, name: 'Baguio City Police Station 6', address: 'Aurora Hill', contact: '300-9116 / 424-2174', image: 'images/locations/policestationsample.jpg', x_coord: '16.426235', y_coord: '120.606191' },
+    { locationID: 14, name: 'Baguio City Police Station 7', address: 'Central Business District', contact: '661-1489', image: 'images/locations/bcpostation7.jpg', x_coord: '16.414404', y_coord: '120.592207' },
+    { locationID: 15, name: 'Baguio City Police Station 8', address: 'Camp 7, Kennon Road', contact: '424-2681', image: 'images/locations/policestationsample.jpg', x_coord: '16.391862', y_coord: '120.599899' },
+    { locationID: 16, name: 'Baguio City Police Station 9', address: 'Irisan', contact: '424-8834', image: 'images/locations/policestationsample.jpg', x_coord: '16.430057', y_coord: '120.548613' },
+    { locationID: 17, name: 'Baguio City Police Station 10', address: 'Green Valley, Marcos Highway', contact: '422-2662', image: 'images/locations/policestationsample.jpg', x_coord: '16.388915', y_coord: '120.575467' },
+    { locationID: 18, name: 'Baguio City Central Fire Station', address: 'No. 1, Corner Abanao and Kayang Streets', contact: '160 / (074) 442-2222 / (074) 443-7089', image: 'images/locations/firestationsample.jpg', x_coord: '16.414757', y_coord: '120.591727' }
 ];
 
 var locationsTrinidad = [
-    { locationID: 1, name: 'Benguet General Hospital', address: 'Halsema Highway, La Trinidad', contact: '(074) 442-3165', image: '', x_coord: '16.450765', y_coord: '120.589133' },
-    { locationID: 2, name: 'Cordillera Hospital of the Divine Grace', address: 'Puguis, La Trinidad', contact: '(074) 620-5692', image: '', x_coord: '16.452586', y_coord: '120.574424' },
-    { locationID: 3, name: 'Camp Dangwa Hospital', address: 'Barangay Alapang, La Trinidad', contact: '(074) 422-5515', image: '', x_coord: '16.464979', y_coord: '120.598705' },
-    { locationID: 4, name: 'Assumption Medical Diagnostic Center', address: 'Bontoc Road, La Trinidad', contact: '(074) 422-3906', image: '', x_coord: '16.446049', y_coord: '120.591062' },
-    { locationID: 5, name: 'Philippine Red Cross', address: 'BeGH Compound, Km. 5 La Trinidad', contact: '(074) 422-2796', image: '', x_coord: '16.450857', y_coord: '120.589391' },
-    { locationID: 6, name: 'La Trinidad Police Station', address: 'Km. 5 Pico, La Trinidad', contact: '(074) 309-1019', image: '', x_coord: '16.447812', y_coord: '120.590238' },
-    { locationID: 7, name: 'Km. 6 Police Outpost', address: 'Km. 6 La Trinidad', contact: '(074) 309-1019', image: '', x_coord: '16.455317', y_coord: '120.589478' },
-    { locationID: 8, name: 'La Trinidad Fire Station', address: 'Cabanao Road, La Trinidad', contact: '(074) 422-4700', image: '', x_coord: '16.460805', y_coord: '120.588766' }
+    { locationID: 1, name: 'Benguet General Hospital', address: 'Halsema Highway, La Trinidad', contact: '(074) 442-3165', image: 'images/locations/benguetgeneralhospital.jpg', x_coord: '16.450765', y_coord: '120.589133' },
+    { locationID: 2, name: 'Cordillera Hospital of the Divine Grace', address: 'Puguis, La Trinidad', contact: '(074) 620-5692', image: 'images/locations/hospitalsample.jpg', x_coord: '16.452586', y_coord: '120.574424' },
+    { locationID: 3, name: 'Camp Dangwa Hospital', address: 'Barangay Alapang, La Trinidad', contact: '(074) 422-5515', image: 'images/locations/hospitalsample.jpg', x_coord: '16.464979', y_coord: '120.598705' },
+    { locationID: 4, name: 'Assumption Medical Diagnostic Center', address: 'Bontoc Road, La Trinidad', contact: '(074) 422-3906', image: 'images/locations/hospitalsample.jpg', x_coord: '16.446049', y_coord: '120.591062' },
+    { locationID: 5, name: 'Philippine Red Cross', address: 'BeGH Compound, Km. 5 La Trinidad', contact: '(074) 422-2796', image: 'images/locations/hospitalsample.jpg', x_coord: '16.450857', y_coord: '120.589391' },
+    { locationID: 6, name: 'La Trinidad Police Station', address: 'Km. 5 Pico, La Trinidad', contact: '(074) 309-1019', image: 'images/locations/bcpocarregionaloffice.jpg', x_coord: '16.447812', y_coord: '120.590238' },
+    { locationID: 7, name: 'Km. 6 Police Outpost', address: 'Km. 6 La Trinidad', contact: '(074) 309-1019', image: 'images/locations/policestationsample.jpg', x_coord: '16.455317', y_coord: '120.589478' },
+    { locationID: 8, name: 'La Trinidad Fire Station', address: 'Cabanao Road, La Trinidad', contact: '(074) 422-4700', image: 'images/locations/firestationsample.jpg', x_coord: '16.460805', y_coord: '120.588766' }
 ];
 
 DBRequest.onerror = function(event) {
@@ -57,8 +50,10 @@ DBRequest.onsuccess = function(event) {
     database = DBRequest.result;
     console.log('Database initialised.');
 
-    getDataBaguio();
-    getDataTrinidad();
+    // getDataBaguio();
+    // getDataTrinidad();
+    getAllBaguioLocations();
+    getAllTrinidadLocations();
 };
 
 DBRequest.onupgradeneeded = function(event) {
@@ -105,48 +100,112 @@ DBRequest.onupgradeneeded = function(event) {
     console.log('Locations object store created.');
 };
 
-//var locationPageObject = document.getElementById('baguioLocation')
-function readAllBaguioLocations() {
+function getAllBaguioLocations() {
+
     console.log('Reading location data in BAGUIO...');
+
     var baguioLocObjStore = database.transaction('baguioLocations').objectStore('baguioLocations');
+
     baguioLocObjStore.openCursor().onsuccess = function(event) {
+        
         var cursor = event.target.result;
+
         if (cursor) {
-            alert(
-                'Baguio Area Emergency Location Information: \n' +
-                'Location ID: ' + cursor.key + '\n' +
-                'Location Name: ' + cursor.value.name + '\n' +
-                'Address: ' + cursor.value.address + '\n' +
-                'Contact Number: ' + cursor.value.contact + '\n' +
-                'Longitude: ' + cursor.value.x_coord + '\n' +
-                'Latitude: ' + cursor.value.y_coord
-            );
+
+            var locationListItem;
+            var name, locationImage, addressHeader, addressInfo, contactHeader, contactInfo;
+            var nameText, addressHeaderText, addressInfoText, contactHeaderText, contactInfoText;
+
+            locationListItem = document.createElement('li');
+            name = document.createElement('h3');
+            locationImage = document.createElement('img');
+            addressHeader = document.createElement('h4');
+            addressInfo = document.createElement('p');
+            contactHeader = document.createElement('h4');
+            contactInfo = document.createElement('p');
+
+            nameText = document.createTextNode(cursor.value.name);
+            addressHeaderText = document.createTextNode('Address: ');
+            addressInfoText = document.createTextNode(cursor.value.address);
+            contactHeaderText = document.createTextNode('Contact Number: ');
+            contactInfoText = document.createTextNode(cursor.value.contact);
+
+            locationImage.setAttribute('src', cursor.value.image);
+            locationImage.setAttribute('alt', cursor.value.name);
+
+            name.appendChild(nameText);
+            addressHeader.appendChild(addressHeaderText);
+            addressInfo.appendChild(addressInfoText);
+            contactHeader.appendChild(contactHeaderText);
+            contactInfo.appendChild(contactInfoText);
+
+            locationListItem.appendChild(name);
+            locationListItem.appendChild(locationImage);
+            locationListItem.appendChild(addressHeader);
+            locationListItem.appendChild(addressInfo);
+            locationListItem.appendChild(contactHeader);
+            locationListItem.appendChild(contactInfo);
+
+            document.getElementById('baguioLocationList').appendChild(locationListItem);
+
             cursor.continue();
 
-            //locationPageObject.innerHTML += 
-            //    '<h3>' + dbGetRequest.result.name + '</h3>';
         } else {
             console.log('[BAGUIO LOCATIONS] Data access completed.');
         };
     };
 };
 
-function readAllTrinidadLocations() {
+function getAllTrinidadLocations() {
+
     console.log('Reading location data in TRINIDAD...');
+
     var trinidadLocObjStore = database.transaction('trinidadLocations').objectStore('trinidadLocations');
+
     trinidadLocObjStore.openCursor().onsuccess = function(event) {
+
         var cursor = event.target.result;
+
         if (cursor) {
-            alert(
-                'La Trinidad Area Emergency Location Information: \n' +
-                'Location ID: ' + cursor.key + '\n' +
-                'Location Name: ' + cursor.value.name + '\n' +
-                'Address: ' + cursor.value.address + '\n' +
-                'Contact Number: ' + cursor.value.contact + '\n' +
-                'Longitude: ' + cursor.value.x_coord + '\n' +
-                'Latitude: ' + cursor.value.y_coord
-            );
+
+            var locationListItem;
+            var name, locationImage, addressHeader, addressInfo, contactHeader, contactInfo;
+            var nameText, addressHeaderText, addressInfoText, contactHeaderText, contactInfoText;
+
+            locationListItem = document.createElement('li');
+            name = document.createElement('h3');
+            locationImage = document.createElement('img');
+            addressHeader = document.createElement('h4');
+            addressInfo = document.createElement('p');
+            contactHeader = document.createElement('h4');
+            contactInfo = document.createElement('p');
+
+            nameText = document.createTextNode(cursor.value.name);
+            addressHeaderText = document.createTextNode('Address: ');
+            addressInfoText = document.createTextNode(cursor.value.address);
+            contactHeaderText = document.createTextNode('Contact Number: ');
+            contactInfoText = document.createTextNode(cursor.value.contact);
+
+            locationImage.setAttribute('src', cursor.value.image);
+            locationImage.setAttribute('alt', cursor.value.name);
+
+            name.appendChild(nameText);
+            addressHeader.appendChild(addressHeaderText);
+            addressInfo.appendChild(addressInfoText);
+            contactHeader.appendChild(contactHeaderText);
+            contactInfo.appendChild(contactInfoText);
+
+            locationListItem.appendChild(name);
+            locationListItem.appendChild(locationImage);
+            locationListItem.appendChild(addressHeader);
+            locationListItem.appendChild(addressInfo);
+            locationListItem.appendChild(contactHeader);
+            locationListItem.appendChild(contactInfo);
+
+            document.getElementById('trinidadLocationList').appendChild(locationListItem);
+            
             cursor.continue();
+
         } else {
             console.log('[TRINIDAD LOCATIONS] Data access completed.');
         };
@@ -237,10 +296,6 @@ function readTrinidadLocation() {
     };
 }
 
-//var bLocationName = document.getElementById('bLocationName');
-//var bLocationAddress = document.getElementById('bLocationAddress');
-//var bContactNumber = document.getElementById('bContactNumber');
-
 function getDataBaguio() {
 
     var input = document.getElementById('locationBaguioInput').value;
@@ -258,10 +313,6 @@ function getDataBaguio() {
             var locationListItem;
             var name, addressHeader, addressInfo, contactHeader, contactInfo;
             var nameText, addressHeaderText, addressInfoText, contactHeaderText, contactInfoText;
-
-            //bLocationName.innerHTML += dbGetRequest.result.name;
-            //bLocationAddress.innerHTML += dbGetRequest.result.address;
-            //bContactNumber.innerHTML += dbGetRequest.result.contact;
 
             locationListItem = document.createElement('li');
             name = document.createElement('h3');
@@ -293,10 +344,6 @@ function getDataBaguio() {
     };
 };
 
-//var tLocationName = document.getElementById('tLocationName');
-//var tLocationAddress = document.getElementById('tLocationAddress');
-//var tContactNumber = document.getElementById('tContactNumber');
-
 function getDataTrinidad() {
 
     var input = document.getElementById('locationTrinidadInput').value;
@@ -314,10 +361,6 @@ function getDataTrinidad() {
             var locationListItem;
             var name, addressHeader, addressInfo, contactHeader, contactInfo;
             var nameText, addressHeaderText, addressInfoText, contactHeaderText, contactInfoText;
-
-            //tLocationName.innerHTML += dbGetRequest.result.name;
-            //tLocationAddress.innerHTML += dbGetRequest.result.address;
-            //tContactNumber.innerHTML += dbGetRequest.result.contact;
 
             locationListItem = document.createElement('li');
             name = document.createElement('h3');
